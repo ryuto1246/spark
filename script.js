3d52f6710815be6f4798b87fcfa25520cc70695e
ryuto1246 <ryutonakamura1246@gmail.com>
2021-03-21 18:25:31 +0900

    [add] questions, seniors, join, contact, footer section

--- script.js
+++ script.js
@@ -5,4 +5,11 @@
   document.getElementById("pageForCompany").addEventListener("click", () => {
     document.getElementsByTagName("body")[0].classList="company"
   });
+
+  document.querySelectorAll(".question-item").forEach(el => {
+    el.addEventListener("click", () => {
+      el.classList.toggle("turned")
+    })
+  });
+
 })();
