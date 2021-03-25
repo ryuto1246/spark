0087ee8570d962cb41cbfcea8b986b387c78ed20
ryuto1246 <ryutonakamura1246@gmail.com>
2021-03-25 20:40:00 +0900

    [add] auto scroll

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 (() => {
+  const selectPageTarget = t => {
+    document.getElementsByTagName("body")[0].classList = t + " fadeout";
+    setTimeout(() => {
+      window.scroll(0, window.innerHeight);
+      document.getElementsByTagName("body")[0].classList = t + " fadein";
+    }, 200);
+  }
+
   document.getElementById("pageForStudents").addEventListener("click", () => {
-    document.getElementsByTagName("body")[0].classList="students"
+    selectPageTarget("students")
   });
   document.getElementById("pageForCompany").addEventListener("click", () => {
-    document.getElementsByTagName("body")[0].classList="company"
+    selectPageTarget("company")
+
   });
 
   document.querySelectorAll(".question-item").forEach(el => {
     el.addEventListener("click", () => {
-      el.classList.toggle("turned")
+      el.classList.toggle("turned");
     })
   });
-
 })();
